dd4358224e673d399ad0d9c1bc50cf82c66eb333
Brandon <crandon1327@gmail.com>
2025-02-26 15:42:05 -0600

    experimenting with tone synth methods

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,45 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useRef, useState } from "react";
 import * as Tone from "tone";
 import "./App.css";
 
+interface Note {
+  name: string;
+  duration: string;
+}
+
+function Key({
+  note,
+  synth,
+  synthRef,
+}: {
+  note: Note;
+  synth: Tone.AMSynth;
+  synthRef: React.RefObject<Tone.AMSynth | null>;
+}) {
+  return (
+    <>
+      <div>
+        <button
+          onMouseDown={() => synth?.triggerAttack(note.name, note.duration)}
+          onMouseUp={() => synth?.triggerRelease()}
+          onMouseLeave={() => synth?.triggerRelease()}
+        >
+          {note.name}
+        </button>
+      </div>
+    </>
+  );
+}
+
 function App() {
-  const synth = new Tone.PolySynth(Tone.Synth).toDestination();
+  const synth = new Tone.AMSynth().toDestination();
+  const synthRef = useRef<Tone.AMSynth | null>(null);
+
   const [count, setCount] = useState(0);
+  const [currSynthName, setCurrSynthName] = useState(synth.name);
   const [isActive, setIsActive] = useState(false);
 
   synth.debug = true;
-
-  interface Note {
-    name: string;
-    duration: string;
-  }
 
   const notes: Note[] = [
     { name: "C3", duration: "8n" },
@@ -27,53 +52,74 @@ function App() {
     { name: "C4", duration: "8n" },
   ];
 
-  const toggleNote = (note: Note) => {
-    if (isActive) {
-      synth.triggerRelease(note.name);
+  useEffect(() => {
+    synthRef.current = new Tone.AMSynth().toDestination();
+    return () => {
+      synthRef.current?.dispose();
+    };
+  }, []);
+
+  const SynthRelease = () => {
+    synthRef.current?.triggerRelease();
+  };
+
+  const handleToggle = (note: Note) => {
+    if (!isActive) {
+      synthRef.current?.triggerAttack(note.name, note.duration, 0.8);
     } else {
-      synth.triggerAttack(note.name, note.duration);
+      synthRef.current?.triggerRelease();
     }
+    console.log(synthRef.current);
+    console.log(`note ${note.name}: ${isActive}`);
     setIsActive(!isActive);
-    console.log(isActive);
   };
 
   const listNotes = notes.map((note: Note) => (
-    <button onClick={() => toggleNote(note)}>{note.name}</button>
+    <button
+      key={note.name}
+      // onClick={() => handleToggle(note)}
+      onMouseDown={() =>
+        synthRef.current?.triggerAttack(note.name, note.duration)
+      }
+      onMouseUp={() => synthRef.current?.triggerRelease()}
+      onMouseLeave={() => synthRef.current?.triggerRelease()}
+    >
+      {note.name}
+    </button>
   ));
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Current Synth: {currSynthName}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
 
         <button
-          //Play note C4 for a duration of an 8th note
-          onClick={() => {
+          onMouseDown={() => {
             synth.triggerAttack("C4", "8n");
           }}
+          onMouseUp={() => synth.triggerRelease()}
         >
           Start Sound
         </button>
         <button
           //Play note C4 for a duration of an 8th note
           onClick={() => {
-            synth.triggerRelease("C4");
+            synth.triggerRelease();
           }}
         >
           Stop Sound
         </button>
       </div>
+
+      <Key
+        note={{ name: "C4", duration: "8n" }}
+        synth={synth}
+        synthRef={synthRef}
+      ></Key>
+
       <div>{listNotes}</div>
     </>
   );
