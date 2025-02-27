6e8e2f8f2b23bc3dd99d4c4cfd6afc0634fc5db7
Brandon <crandon1327@gmail.com>
2025-02-27 16:33:28 -0600

    more keys

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useEffect, useRef, useState } from "react";
 import * as Tone from "tone";
 import "./App.css";
 
+import { Slider } from "@/components/ui/slider";
+
 interface Note {
   name: string;
   duration: string;
@@ -36,12 +38,23 @@ function App() {
   const synthRef = useRef<Tone.AMSynth | null>(null);
 
   const [count, setCount] = useState(0);
+
+  const [sliderValue, setSliderValue] = useState([40]);
+
   const [currSynthName, setCurrSynthName] = useState(synth.name);
   const [isActive, setIsActive] = useState(false);
 
   synth.debug = true;
 
+  console.log(synthRef.current?.oscillator.type.toString());
   const notes: Note[] = [
+    { name: "C2", duration: "8n" },
+    { name: "D2", duration: "8n" },
+    { name: "E2", duration: "8n" },
+    { name: "F2", duration: "8n" },
+    { name: "G2", duration: "8n" },
+    { name: "A2", duration: "8n" },
+    { name: "B2", duration: "8n" },
     { name: "C3", duration: "8n" },
     { name: "D3", duration: "8n" },
     { name: "E3", duration: "8n" },
@@ -50,12 +63,22 @@ function App() {
     { name: "A4", duration: "8n" },
     { name: "B4", duration: "8n" },
     { name: "C4", duration: "8n" },
+    { name: "D4", duration: "8n" },
+    { name: "E4", duration: "8n" },
+    { name: "F4", duration: "8n" },
+    { name: "G4", duration: "8n" },
+    { name: "A5", duration: "8n" },
+    { name: "B5", duration: "8n" },
   ];
 
   useEffect(() => {
     synthRef.current = new Tone.AMSynth().toDestination();
+    // const chorus = new Tone.Chorus(4, 2.5, 0.5).toDestination().start();
+    // synthRef.current?.connect(chorus);
+    console.log("again?");
     return () => {
       synthRef.current?.dispose();
+      console.log("disposed?");
     };
   }, []);
 
@@ -121,6 +144,35 @@ function App() {
       ></Key>
 
       <div>{listNotes}</div>
+
+      <div>
+        <Slider
+          label="Frequency Slider"
+          width={"200px"}
+          defaultValue={[40]}
+          value={sliderValue}
+          onValueChange={(e) => setSliderValue(e.value)}
+        ></Slider>
+
+        <p>{sliderValue}</p>
+      </div>
+
+      <div>
+        <ul>
+          <li>
+            <p>{synthRef.current?.name}</p>
+          </li>
+          <li>
+            <p>{synthRef.current?.name}</p>
+          </li>
+          <li>
+            <p>{synth.output.toString()}</p>
+          </li>
+          <li>
+            <p>{synth.volume.value.toString()}</p>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
